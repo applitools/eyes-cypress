e72c85ef68d5279423b8f0cb237e433e924806d2
Amit Zur <sendwithchibo@gmail.com>
2018-04-30 14:49:19 +0300

    tests against real applitools server

--- tests/openEyes.test.js
+++ tests/openEyes.test.js
@@ -1,10 +1,17 @@
 'use strict';
+require('dotenv').config();
 const {describe, it, before, after} = require('mocha');
 const {expect} = require('chai');
 const openEyes = require('../src/server/openEyes');
 const FakeEyesWrapper = require('./FakeEyesWrapper');
 const testServer = require('./testServer');
 const domNodesToCdt = require('../src/client/domNodesToCdt');
+const {DiffsFoundError} = require('@applitools/eyes.sdk.core');
+
+function loadJson(filepath) {
+  const json = require(filepath);
+  return JSON.parse(JSON.stringify(json));
+}
 
 describe('openEyes', () => {
   let baseUrl, closeServer;
@@ -36,7 +43,7 @@ describe('openEyes', () => {
       url: `${baseUrl}/test.html`,
     });
     const resourceUrls = ['smurfs.jpg', 'test.css'];
-    const cdt = require('./fixtures/test.cdt.json');
+    const cdt = loadJson('./fixtures/test.cdt.json');
     const result = await checkWindow({resourceUrls, cdt});
     expect(result.getAsExpected()).to.equal(true);
     await close();
@@ -48,10 +55,47 @@ describe('openEyes', () => {
       url: `${baseUrl}/test.html`,
     });
     const resourceUrls = ['smurfs.jpg', 'test.css'];
-    const cdt = require('./fixtures/test.cdt.json');
+    const cdt = loadJson('./fixtures/test.cdt.json');
     cdt.find(node => node.nodeValue === "hi, I'm red").nodeValue = "hi, I'm green";
     const result = await checkWindow({resourceUrls, cdt});
     expect(result.getAsExpected()).to.equal(false);
     await close();
+  });
+
+  it('really works', async () => {
+    const {checkWindow, close} = openEyes({
+      appName: 'some app',
+      testName: 'some test',
+      apiKey: process.env.APPLITOOLS_API_KEY, // TODO bad for tests. what to do
+      url: `${baseUrl}/test.html`,
+      viewportSize: {width: 800, height: 600},
+    });
+
+    const resourceUrls = ['smurfs.jpg', 'test.css'];
+    const cdt = loadJson('./fixtures/test.cdt.json');
+    await checkWindow({resourceUrls, cdt});
+    await close();
+  });
+
+  it('really works and fails', async () => {
+    const {checkWindow, close} = openEyes({
+      appName: 'some app',
+      testName: 'some test',
+      apiKey: process.env.APPLITOOLS_API_KEY, // TODO bad for tests. what to do
+      url: `${baseUrl}/test.html`,
+      viewportSize: {width: 800, height: 600},
+    });
+
+    const resourceUrls = ['smurfs.jpg', 'test.css'];
+    const cdt = loadJson('./fixtures/test.cdt.json');
+    cdt.find(node => node.nodeValue === "hi, I'm red").nodeValue = "hi, I'm green";
+    await checkWindow({resourceUrls, cdt});
+    try {
+      await close();
+    } catch (ex) {
+      if (!(ex instanceof DiffsFoundError)) {
+        throw ex;
+      }
+    }
   });
 });
