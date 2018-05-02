68f90c7f047e7e512ee1a01bb226165588c0b838
Amit Zur <sendwithchibo@gmail.com>
2018-05-02 11:47:53 +0300

    change fetchResources test to work with server

--- tests/fetchResources.test.js
+++ tests/fetchResources.test.js
@@ -1,36 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const {describe, it, before, after} = require('mocha');
 const fetchResources = require('../src/server/fetchResources');
 const {expect} = require('chai');
-const nock = require('nock');
 const {mapValues} = require('lodash');
+const testServer = require('./testServer');
 
 describe('fetchResources', () => {
+  let baseUrl, closeServer;
+  before(async () => {
+    const server = await testServer();
+    baseUrl = `http://localhost:${server.port}`;
+    closeServer = server.close;
+  });
+
+  after(() => {
+    closeServer();
+  });
+
   it('works', async () => {
+    const jpgName = 'smurfs.jpg';
+    const cssName = 'test.css';
+    const jsonName = 'test.cdt.json';
+    const jsName = 'test.js';
+    const jpgUrl = `${baseUrl}/${jpgName}`;
+    const cssUrl = `${baseUrl}/${cssName}`;
+    const jsonUrl = `${baseUrl}/${jsonName}`;
+    const jsUrl = `${baseUrl}/${jsName}`;
+    const jpgContent = fs.readFileSync(path.resolve(__dirname, 'fixtures', jpgName));
+    const cssContent = fs.readFileSync(path.resolve(__dirname, 'fixtures', cssName));
+    const jsonContent = fs.readFileSync(path.resolve(__dirname, 'fixtures', jsonName));
+    const jsContent = fs.readFileSync(path.resolve(__dirname, 'fixtures', jsName));
+
     const expected = mapValues(
       {
-        'http://some/url.png': {type: 'image/png', value: 'some image'},
-        'http://some/url.css': {type: 'text/css', value: 'some css'},
-        'http://some/url.json': {type: 'application/json', value: 'some json'},
-        'http://some/url.js': {type: 'application/javascript', value: 'some javascript'},
+        [jpgUrl]: {type: 'image/jpeg', value: jpgContent},
+        [cssUrl]: {type: 'text/css; charset=UTF-8', value: cssContent},
+        [jsonUrl]: {type: 'application/json; charset=UTF-8', value: jsonContent},
+        [jsUrl]: {type: 'application/javascript; charset=UTF-8', value: jsContent},
       },
-      (o, url) => ({type: o.type, value: Buffer.from(o.value, 'utf-8'), url}),
+      (o, url) => ({type: o.type, value: o.value, url}),
     );
 
-    nock('http://some')
-      .get('/url.png')
-      .reply(200, 'some image', {'Content-Type': 'image/png'})
-      .get('/url.css')
-      .reply(200, 'some css', {'Content-Type': 'text/css'})
-      .get('/url.json')
-      .reply(200, 'some json', {'Content-Type': 'application/json'})
-      .get('/url.js')
-      .reply(200, 'some javascript', {'Content-Type': 'application/javascript'});
-
-    const resources = await fetchResources([
-      'http://some/url.png',
-      'http://some/url.css',
-      'http://some/url.json',
-      'http://some/url.js',
-    ]);
+    const resources = await fetchResources([jpgUrl, cssUrl, jsonUrl, jsUrl]);
 
     expect(resources).to.deep.equal(expected);
   });
