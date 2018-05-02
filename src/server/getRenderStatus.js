649dac65ce102e8a0f267239b00b59454310e22e
Amit Zur <sendwithchibo@gmail.com>
2018-05-02 16:33:05 +0300

    refactor out getRenderStatus

--- src/server/getRenderStatus.js
+++ src/server/getRenderStatus.js
@@ -1,7 +1,35 @@
-async function getRenderStatus(renderIds) {
-  // TODO send POST to /render-status
-  // TODO remove completed renderIds from array
-  // TODO call matchWindow on completed renderIds
+const {RenderStatus} = require('@applitools/eyes.sdk.core');
+
+const GET_STATUS_INTERVAL = 500; // TODO take from SDK?
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+async function getRenderStatus(renderId, doGetRenderStatus) {
+  try {
+    const renderStatus = await doGetRenderStatus(renderId);
+    const status = renderStatus.getStatus();
+    if (status === RenderStatus.RENDERING) {
+      await sleep(GET_STATUS_INTERVAL);
+      return await getRenderStatus(renderId, doGetRenderStatus);
+    } else if (status === RenderStatus.ERROR) {
+      throw renderStatus.getError();
+    }
+    return renderStatus.getImageLocation();
+  } catch (ex) {
+    // TODO number of retries?
+    await sleep(GET_STATUS_INTERVAL); // TODO use GeneralUtils from SDK?
+    return await getRenderStatus(renderId, doGetRenderStatus);
+  }
 }
 
 module.exports = getRenderStatus;
+
+/*****
+ * TODO: when this is rewritten to be optimized, it should query status for multiple renderIds
+
+  async function getRenderStatus(renderIds) {
+    // TODO send POST to /render-status (_serverConnector.getRenderStatusByIds)
+    // TODO remove completed renderIds from array
+    // TODO call matchWindow on completed renderIds
+  }
+
+ */
--- src/server/openEyes.js
+++ src/server/openEyes.js
@@ -1,9 +1,6 @@
 const EyesWrapper = require('./EyesWrapper');
 const getAllResources = require('./getAllResources');
-const {RenderStatus, GeneralUtils} = require('@applitools/eyes.sdk.core');
-
-const GET_STATUS_INTERVAL = 500; // TODO take from SDK?
-const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+const getRenderStatus = require('./getRenderStatus');
 
 function openEyes({
   appName,
@@ -28,31 +25,12 @@ function openEyes({
       renderWidth,
       renderInfo,
     });
-    const screenshotUrl = await getRenderStatus(renderId);
+    const screenshotUrl = await getRenderStatus(renderId, wrapper.getRenderStatus.bind(wrapper));
     return await wrapper.checkWindow({screenshotUrl, tag});
   }
 
   async function close() {
     return await wrapper.close();
-  }
-
-  // TODO move to other file
-  async function getRenderStatus(renderId) {
-    try {
-      const renderStatus = await wrapper.getRenderStatus(renderId);
-      const status = renderStatus.getStatus();
-      if (status === RenderStatus.RENDERING) {
-        await sleep(GET_STATUS_INTERVAL);
-        return getRenderStatus(renderId);
-      } else if (status === RenderStatus.ERROR) {
-        throw renderStatus.getError();
-      }
-      return renderStatus.getImageLocation();
-    } catch (ex) {
-      // TODO number of retries?
-      await sleep(GET_STATUS_INTERVAL); // TODO use GeneralUtils from SDK?
-      return getRenderStatus(renderId);
-    }
   }
 
   wrapper.open(appName, testName, viewportSize);
