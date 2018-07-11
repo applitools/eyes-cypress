713be12a53ceb057e2a902aeffbbdff07a672d66
Amit Zur <sendwithchibo@gmail.com>
2018-07-11 17:18:03 +0300

    remove unnecessary dependencies

--- src/cypress/plugin/server.js
+++ src/cypress/plugin/server.js
@@ -1,6 +1,5 @@
 'use strict';
 const express = require('express');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const {promisify: p} = require('util');
@@ -34,28 +33,28 @@ async function closeEyes() {
 
 const app = express();
 app.use(cors());
-if (process.env.APPLITOOLS_DEV_MODE) app.use(morgan('combined'));
 app.get('/hb', (_req, res) => res.sendStatus(200));
 app.get('/err', (_req, res) => res.status(500).send('this is a test error'));
 
 app.put('/eyes/resource/:id', bodyParser.raw({type: '*/*', limit: '100mb'}), async (req, res) => {
   try {
+    logger.log('[server] PUT resource:', req.params.id);
     handlers.putResource(req.params.id, Buffer.from(JSON.parse(req.body).data));
     res.status(200).send({success: true});
   } catch (ex) {
-    console.error('error in PUT resource', req.params && req.params.id, ex);
+    logger.log('[server] error in PUT resource', req.params && req.params.id, ex);
     res.status(200).send({success: false, error: ex.message});
   }
 });
 
 app.post('/eyes/:command', express.json({limit: '100mb'}), async (req, res) => {
-  logger.log(`eyes api: ${req.params.command}`, Object.keys(req.body));
+  logger.log(`[server] eyes api: ${req.params.command}`, Object.keys(req.body));
   try {
     const result = await handlers[req.params.command](req.body);
     res.set('Content-Type', 'application/json');
     res.status(200).send({success: true, result});
   } catch (ex) {
-    logger.log('error in eyes api:', ex);
+    logger.log('[server] error in eyes api:', ex);
     res.status(200).send({success: false, error: ex.message});
   }
 });
