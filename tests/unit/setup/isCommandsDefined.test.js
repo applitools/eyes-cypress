04f16e7ebf9c761d7c42388d3f0b0fec3b06df5d
Amit Zur <sendwithchibo@gmail.com>
2018-08-01 14:03:09 +0300

    fix isCommands defined

---
+++ tests/unit/setup/isCommandsDefined.test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const {describe, it} = require('mocha');
+const {expect} = require('chai');
+const isCommandsDefined = require('../../../src/cypress/setup/isCommandsDefined');
+
+describe.only('isCommandsDefined', () => {
+  it('handles single quote require', () => {
+    const text = "require('@applitools/eyes.cypress/commands');";
+    expect(isCommandsDefined(text)).to.equal(true);
+  });
+
+  it('handles double quote require', () => {
+    const text = 'require("@applitools/eyes.cypress/commands");';
+    expect(isCommandsDefined(text)).to.equal(true);
+  });
+
+  it('handles require with spaces', () => {
+    const text = 'require ( "@applitools/eyes.cypress/commands" ) ;';
+    expect(isCommandsDefined(text)).to.equal(true);
+  });
+
+  it('handles multiline with require', () => {
+    const text = `
+    'use strict';
+
+    require('@applitools/eyes.cypress/commands');
+
+    import './commands'
+    
+    `;
+
+    expect(isCommandsDefined(text)).to.equal(true);
+  });
+
+  it('handles single quote import', () => {
+    const text = "import '@applitools/eyes.cypress/commands'";
+    expect(isCommandsDefined(text)).to.equal(true);
+  });
+
+  it('handles double quote import', () => {
+    const text = 'import "@applitools/eyes.cypress/commands"';
+    expect(isCommandsDefined(text)).to.equal(true);
+  });
+
+  it('handles import with spaces', () => {
+    const text = 'import   "@applitools/eyes.cypress/commands"    ;   ';
+    expect(isCommandsDefined(text)).to.equal(true);
+  });
+
+  it('handles multiline with import', () => {
+    const text = `
+    'use strict';
+
+    import '@applitools/eyes.cypress/commands'
+
+    import './commands'
+    
+    `;
+
+    expect(isCommandsDefined(text)).to.equal(true);
+  });
+
+  it('handles wrong inner require', () => {
+    const text = 'require("@applitools/eyes.cypress/bla");';
+    expect(isCommandsDefined(text)).to.equal(false);
+  });
+
+  it('handles wrong require', () => {
+    const text = 'require("@applitools/eyes.cypress");';
+    expect(isCommandsDefined(text)).to.equal(false);
+  });
+});
