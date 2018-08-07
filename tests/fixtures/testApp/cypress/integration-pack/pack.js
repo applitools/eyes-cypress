69f0458474a22349d034addeae7cf01526b9cd82
Amit Zur <sendwithchibo@gmail.com>
2018-08-07 18:37:41 +0300

    before hook

--- tests/fixtures/testApp/cypress/integration-pack/pack.js
+++ tests/fixtures/testApp/cypress/integration-pack/pack.js
@@ -1,16 +1,19 @@
 describe('eyes.cypress', () => {
 
   // This also tests the setting of `testName` in `before`
-
-  it('pack', () => {
-    cy.setCookie('auth', 'secret');
-    const url = `http://localhost:${Cypress.config('testPort')}/test.html`;
-    cy.visit(url);
+  
+  before(() => {
     cy.eyesOpen({
       appName: 'some app',
       browser: {width: 1024, height: 768},
       showLogs: true
     });
+  });
+
+  it('pack', () => {
+    cy.setCookie('auth', 'secret');
+    const url = `http://localhost:${Cypress.config('testPort')}/test.html`;
+    cy.visit(url);
     cy.eyesCheckWindow();
     cy.eyesClose();
   });
