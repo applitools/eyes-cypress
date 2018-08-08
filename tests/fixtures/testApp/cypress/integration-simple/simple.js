dfc82cb9bbd16293151ffc878cf827fb0708cf8a
Amit Zur <sendwithchibo@gmail.com>
2018-08-08 12:52:56 +0300

    playing with tests

--- tests/fixtures/testApp/cypress/integration-simple/simple.js
+++ tests/fixtures/testApp/cypress/integration-simple/simple.js
@@ -1,10 +1,10 @@
 /* globals describe,it,cy,Cypress */
+after(() => {
+  cy.then(() => {
+    throw new Error('aaa');
+  });
+});
 describe('eyes.cypress', () => {
-  // after(() => {
-  //   cy.then(() => {
-  //     throw new Error('aaa');
-  //   });
-  // });
   it('simple', () => {
     const url = `http://localhost:${Cypress.config('testPort')}/test.html`;
     cy.visit(url);
