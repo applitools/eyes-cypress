b5fcd95dd6e9468f913441dcee7c65299880a01b
Amit Zur <sendwithchibo@gmail.com>
2018-10-24 14:42:56 +0300

    refactor getUrls in play task

--- tests/fixtures/testApp/cypress/plugins/index-play.js
+++ tests/fixtures/testApp/cypress/plugins/index-play.js
@@ -1,25 +1,13 @@
 // eslint-disable-next-line
 const fetch = require('node-fetch');
 
-function uniq(arr) {
-  return Array.from(new Set(arr));
-}
-
 async function getUrls() {
   const sitemapUrl = 'http://a142332.hostedsitemap.com/4049686/urllist.txt';
   const filters = [/\/blog/, /\.png/, /\.pdf/];
 
   const resp = await fetch(sitemapUrl);
   const text = await resp.text();
-  return uniq(text.split(/\n/g))
-    .filter(u => !filters.some(f => u.match(f)) && u.includes('/docs'))
-    .map(u => {
-      let qpIndex = u.indexOf('?');
-      if (qpIndex > -1) {
-        return u.substring(0, qpIndex);
-      }
-      return u;
-    });
+  return text.split(/\n/g).filter(u => !filters.some(f => u.match(f)) && u.includes('/docs'));
 }
 
 // eslint-disable-next-line
