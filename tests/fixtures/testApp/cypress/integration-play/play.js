f54a4e71a324f230a8bdb9327101df8e55f1a0a0
Amit Zur <sendwithchibo@gmail.com>
2018-11-28 15:23:37 +0200

    play around

--- tests/fixtures/testApp/cypress/integration-play/play.js
+++ tests/fixtures/testApp/cypress/integration-play/play.js
@@ -2,6 +2,10 @@
 describe('random play', () => {
   it('random play', () => {
     cy.visit('https://theintercept.com/privacy-policy/');
+
+    // const url = `http://localhost:${Cypress.config('testPort')}/test.html`;
+    // cy.visit(url);
+
     cy.eyesOpen({
       appName: 'cypress play around',
     });
