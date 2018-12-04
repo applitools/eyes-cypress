e6611b63f95daf50e1461077177e8e8f250f185b
Amit Zur <sendwithchibo@gmail.com>
2018-12-04 16:31:09 +0200

    play stuff

--- tests/fixtures/testApp/cypress/integration-play/play.js
+++ tests/fixtures/testApp/cypress/integration-play/play.js
@@ -1,7 +1,7 @@
 /* globals describe,it,cy */
 describe('random play', () => {
   it('random play', () => {
-    cy.visit('http://applitools.github.io/demo/TestPages/FramesTestPage/');
+    cy.visit('http://localhost:3000');
 
     // const url = `http://localhost:${Cypress.config('testPort')}/test.html`;
     // cy.visit(url);
@@ -9,27 +9,7 @@ describe('random play', () => {
     cy.eyesOpen({
       appName: 'cypress play around',
     });
-    cy.eyesCheckWindow({
-      floating: [
-        {
-          top: 79.875,
-          left: 8,
-          width: 304,
-          height: 184,
-          maxUpOffset: 20,
-          maxDownOffset: 20,
-          maxLeftOffset: 2,
-          maxRightOffset: 20,
-        }, //coordinates and offsets
-        {
-          selector: '#overflowing-div-image',
-          maxUpOffset: 20,
-          maxDownOffset: 20,
-          maxLeftOffset: 2,
-          maxRightOffset: 20,
-        },
-      ], //CSS selector and offsets]
-    });
+    cy.eyesCheckWindow();
     cy.eyesClose();
   });
 });
