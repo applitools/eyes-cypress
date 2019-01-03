2710fc01b249232f0b981ef1ee405271fc18b184
Daniel Schwartz <danielsch24@gmail.com>
2019-01-03 15:20:18 +0200

    First commit whoo - WIP - disabling all cmds in the browser if .

--- src/browser/commands.js
+++ src/browser/commands.js
@@ -10,30 +10,41 @@ const sendRequest = makeSendRequest(send);
 const Blob = window.frameElement.ownerDocument.defaultView.Blob; // yucky! cypress uses socket.io to communicate between browser and node. In order to encode the data in binary format, socket.io checks for binary values. But `value instanceof Blob` is falsy since Blob from the cypress runner window is not the Blob from the command's window. So using the Blob from cypress runner window here.
 const eyesCheckWindow = makeEyesCheckWindow({sendRequest, processPage, Blob});
 
-const batchEnd = poll(function({timeout}) {
-  return sendRequest({command: 'batchEnd', data: {timeout}});
-});
+if (!Cypress.config('eyesIsDisabled')) {
+  const batchEnd = poll(function({timeout}) {
+    return sendRequest({command: 'batchEnd', data: {timeout}});
+  });
 
-before(() => {
-  sendRequest({command: 'batchStart'});
-});
+  before(() => {
+    sendRequest({command: 'batchStart'});
+  });
 
-after(() => {
-  return batchEnd({timeout: Cypress.config('eyesTimeout')});
-});
+  after(() => {
+    return batchEnd({timeout: Cypress.config('eyesTimeout')});
+  });
+}
+
+let isCurrentTestDisabled;
 
 Cypress.Commands.add('eyesOpen', function(args = {}) {
   const {title: testName} = this.currentTest || this.test;
   Cypress.log({name: 'Eyes: open'});
+  isCurrentTestDisabled = Cypress.config('eyesIsDisabled') || args.isDisabled;
+  if (isCurrentTestDisabled) return;
   return sendRequest({command: 'open', data: Object.assign({testName}, args)});
 });
 
 Cypress.Commands.add('eyesCheckWindow', args => {
   Cypress.log({name: 'Eyes: check window'});
+  if (isCurrentTestDisabled) return;
   return cy.document({log: false}).then({timeout: 60000}, doc => eyesCheckWindow(doc, args));
 });
 
 Cypress.Commands.add('eyesClose', () => {
   Cypress.log({name: 'Eyes: close'});
+  if (isCurrentTestDisabled) {
+    isCurrentTestDisabled = false;
+    return;
+  }
   return sendRequest({command: 'close'});
 });
--- src/plugin/pluginExport.js
+++ src/plugin/pluginExport.js
@@ -1,14 +1,16 @@
 'use strict';
 
-function makePluginExport(startServer) {
+function makePluginExport({startServer, getConfig}) {
   return function pluginExport(pluginModule) {
     let closeEyesServer;
     const pluginModuleExports = pluginModule.exports;
     pluginModule.exports = async (...args) => {
-      const {eyesPort, closeServer} = await startServer();
+      const {eyesPort, closeServer} = await startServer({getConfig});
+
       closeEyesServer = closeServer;
       const moduleExportsResult = await pluginModuleExports(...args);
-      return Object.assign({eyesPort}, moduleExportsResult);
+      const {isDisabled: eyesIsDisabled} = getConfig();
+      return Object.assign({eyesPort, eyesIsDisabled}, moduleExportsResult);
     };
     return function getCloseServer() {
       return closeEyesServer;
