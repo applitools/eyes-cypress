561284ac6b73721f2ccf08f1e4b970d150270577
Amit Zur <sendwithchibo@gmail.com>
2019-01-16 08:46:05 +0200

    update vgc to get isomorphic version 10

--- src/plugin/startPlugin.js
+++ src/plugin/startPlugin.js
@@ -5,18 +5,14 @@ const agentId = `eyes.cypress/${packageVersion}`;
 const makeStartServer = require('./server');
 const makePluginExport = require('./pluginExport');
 const {startApp} = require('./app');
-const {
-  makeVisualGridClient,
-  makeGetConfig,
-  createLogger,
-} = require('@applitools/visual-grid-client');
+const {ConfigUtils, Logger} = require('@applitools/eyes-common');
+const {makeVisualGridClient, configParams} = require('@applitools/visual-grid-client');
 const getErrorsAndDiffs = require('./getErrorsAndDiffs');
 const processCloseAndAbort = require('./processCloseAndAbort');
 const errorDigest = require('./errorDigest');
 const makeHandlers = require('./handlers');
-const getConfig = makeGetConfig();
-const config = Object.assign({concurrency: 1, agentId}, getConfig());
-const logger = createLogger(config.showLogs);
+const config = Object.assign({concurrency: 1, agentId}, ConfigUtils.getConfig({configParams}));
+const logger = new Logger(config.showLogs);
 const handlers = makeHandlers({
   logger,
   config,
