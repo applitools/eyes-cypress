b05ba3db35a1138bc8a1b1c54fdc3e68c15179b6
Daniel Schwartz <danielsch24@gmail.com>
2019-02-13 17:38:28 +0200

    changed play file

--- tests/fixtures/testApp/cypress/integration-play/play.js
+++ tests/fixtures/testApp/cypress/integration-play/play.js
@@ -4,22 +4,11 @@ describe('Hello world', () => {
     cy.eyesOpen({
       appName: 'Oral',
       testName: 'gils test',
-    }).then(() => {
-      console.error('OPEN DONE');
     });
-    cy.visit('https://beta.oral.fi/palvelut/hammaskiven-poisto/').then(() => {
-      console.error('VISIT DONE');
-    });
+    cy.visit('https://applitools.com/helloworld');
 
-    cy.wait(5000);
     cy.eyesCheckWindow({
       tag: 'Nearby times before click',
-    }).then(() => {
-      console.error('CHECK WINDOW  DONE');
-    });
-
-    cy.get('[aria-controls="panel-time-reservation"]').then(() => {
-      console.error('GET  DONE');
     });
 
     cy.eyesClose();
