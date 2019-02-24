fd95b57ca2a7a72eadee46cb9a969dbac6e9004c
Daniel Schwartz <danielsch24@gmail.com>
2019-02-24 14:29:14 +0200

    removed bad commits

--- tests/fixtures/testApp/cypress/integration-play/play.js
+++ tests/fixtures/testApp/cypress/integration-play/play.js
@@ -2,10 +2,14 @@
 describe('Hello world', () => {
   it('works', () => {
     cy.eyesOpen({
-      appName: 'Oral',
-      testName: 'gils test',
+      appName: 'someApp',
+      testName: 'Check all landing page widgets',
+      browser: [
+        {width: 1440, height: 900, name: 'firefox'},
+        {width: 1440, height: 900, name: 'chrome'},
+      ],
     });
-    cy.visit('https://applitools.com/helloworld');
+    cy.visit('https://www.unibet.com/betting#home');
 
     cy.eyesCheckWindow({
       tag: 'Nearby times before click',
