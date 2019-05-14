908ae2cf27bb07877949e8ed1b6b4ac9a54273b7
Daniel Schwartz <danielsch24@gmail.com>
2019-05-14 11:34:37 +0300

    added render script

---
+++ tests/fixtures/testApp/cypress/plugins/index-render.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line
+const fetch = require('node-fetch');
+
+// eslint-disable-next-line
+const startTestServer = require('../../../../util/testServer');
+
+module.exports = async (on, _config) => {
+  const testServer = await startTestServer();
+  return {testPort: testServer.port};
+};
+
+// eslint-disable-next-line
+require('../../../../../')(module);
---
+++ tests/fixtures/testApp/cypress/render/render.js
@@ -0,0 +1,17 @@
+/* globals describe,it,cy */
+
+const url = Cypress.env('url');
+const testName = `Cypress Render ${url}`;
+
+describe('Cypress Render', () => {
+  it('Cypress Render', () => {
+    cy.eyesOpen({
+      appName: 'Cypress Render',
+      testName: testName,
+      browser: [{width: 1024, height: 768, name: 'chrome'}],
+    });
+    cy.visit(url);
+    cy.eyesCheckWindow({tag: 'After Load'});
+    cy.eyesClose();
+  });
+});
