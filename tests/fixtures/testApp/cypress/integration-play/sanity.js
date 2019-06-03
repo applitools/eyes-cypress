d364fac164818e292052a509de13349485689010
Daniel Schwartz <danielsch24@gmail.com>
2019-06-03 18:02:35 +0300

    23 sanity tests for good easy baseline comparison !

--- tests/fixtures/testApp/cypress/integration-play/sanity.js
+++ tests/fixtures/testApp/cypress/integration-play/sanity.js
@@ -1,6 +1,6 @@
 /* globals describe,it,cy */
 
-const urls = [
+const urls1 = [
   'https://facebook.com',
   'https://twitter.com',
   'https://wikipedia.org',
@@ -13,10 +13,26 @@ const urls = [
   'https://docs.approvesimple.com/docs',
 ];
 
+const urls2 = [
+  'https://theintercept.com/privacy-policy/',
+  'https://www.lidl.com/products?category=OCI1000039&sort=productAtoZ',
+  'http://example.com',
+  'https://www.autogravity.com/inventory?page=0&query=%7B%22bodyStyles%22%3A%5B%7B%22label%22%3A%22SUV%22%2C%22queryValue%22%3A%22SUV%22%7D%5D%2C%22conditions%22%3A%5B%7B%22label%22%3A%22New%22%2C%22queryValue%22%3A%22NEW%22%7D%5D%2C%22regions%22%3A%5B%7B%22queryValue%22%3A%7B%22lat%22%3A40.755322%2C%22lon%22%3A-73.9932872%2C%22rad%22%3A30%7D%7D%5D%2C%22vehicles%22%3A%5B%7B%22label%22%3A%22Kia+Sorento%22%2C%22queryValue%22%3A%7B%22make%22%3A%7B%22id%22%3A19%7D%2C%22model%22%3A%7B%22id%22%3A646%7D%7D%7D%5D%7D&size=50&sort=dealerRank%2Cdesc',
+  'https://www.aetna.com/employers-organizations.html',
+  'https://www.getbridge.com/solutions/human-resources',
+  'https://www.applitools.com/users/login',
+  'http://angiejones.tech/',
+  'https://investor.vanguard.com/home/',
+  'https://southwest.com',
+  'https://www.usaa.com/inet/wc/auto-insurance?wa_ref=pub_global_products_ins_auto',
+  'https://applitools-sample-web-app-testkit.surge.sh/page-with-resource.html',
+  'https://ous.test.clper.me/app/login',
+];
+
 describe('Sanity', () => {
-  for (let url of urls) {
+  for (let url of urls1.concat(urls2)) {
     it(`Sanity - ${url}`, () => {
-      cy.visit(url);
+      cy.visit(url, {failOnStatusCode: false});
       cy.wait(3000);
       cy.eyesOpen({
         appName: 'SanityWeb',
