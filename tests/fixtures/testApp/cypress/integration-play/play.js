b29e8f6d025edcdc6794fe74c8f161a2bd2b014e
Daniel Schwartz <danielsch24@gmail.com>
2019-06-20 11:35:49 +0300

    play changes

--- tests/fixtures/testApp/cypress/integration-play/play.js
+++ tests/fixtures/testApp/cypress/integration-play/play.js
@@ -1,16 +1,17 @@
 /* globals describe,it,cy,Cypress */
 
 Cypress.on('uncaught:exception', () => {});
-const url = 'https://google.com';
+const url = 'https://www.autogravity.com/dealer';
 const testName = url;
 
 describe('Hello world', () => {
   it('works', () => {
     cy.visit(url, {failOnStatusCode: false});
-    cy.wait(5000);
+    cy.wait(1000);
     cy.eyesOpen({
       appName: 'CypressPlay',
       testName: testName,
+      browser: [{width: 1024, height: 768, name: 'chrome'}],
     });
 
     cy.eyesCheckWindow({
