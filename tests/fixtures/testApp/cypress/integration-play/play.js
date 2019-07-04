877f26b8daca67753c571a2b2055b5e6e3f0b41c
Daniel Schwartz <danielsch24@gmail.com>
2019-07-04 15:40:10 +0300

    updated ds to support cssRules getting CORS

--- tests/fixtures/testApp/cypress/integration-play/play.js
+++ tests/fixtures/testApp/cypress/integration-play/play.js
@@ -2,17 +2,17 @@
 
 describe('product-page-applitools', () => {
   it('Abra', () => {
-    cy.visit('https://ous.test.clper.me/app/login', {failOnStatusCode: false});
-    cy.wait(7000);
+    cy.visit(
+      'https://www.asos.com/asos-maternity-nursing/asos-design-maternity-nursing-sleeveless-smock-top-with-crochet-hem/prd/12263487?clr=ecru&colourWayId=16418082&SearchQuery=&cid=27108',
+      {failOnStatusCode: false},
+    );
+    cy.wait(1000);
     cy.eyesOpen({
       appName: 'product-page-applitools',
       browser: {width: 1024, height: 768, name: 'chrome'},
     });
     cy.eyesCheckWindow({
       tag: 'Abra pendant',
-      // scriptHooks: {
-      //   beforeCaptureScreenshot: 'new Promise(r => setTimeout(r, 3000));',
-      // },
     });
     cy.eyesClose();
   });
