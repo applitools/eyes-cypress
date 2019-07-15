ca171491afd366dce07d7f0f223983afb32537b8
Daniel Schwartz <danielsch24@gmail.com>
2019-07-15 17:09:47 +0300

    updated ds: fixed probablyCORS and iframe cssRules

--- tests/fixtures/testApp/cypress/integration-play/play.js
+++ tests/fixtures/testApp/cypress/integration-play/play.js
@@ -1,16 +1,16 @@
 /* global describe, it, cy */
 
+// Cypress.on('uncaught:exception', () => {});
+
 describe('product-page-applitools', () => {
   it('Abra', () => {
-    cy.visit('https://applitools.com/helloworld', {failOnStatusCode: false});
+    cy.visit('https://www.turncar.com/', {failOnStatusCode: false});
     cy.wait(1000);
     cy.eyesOpen({
       appName: 'product-page-applitools',
       browser: {width: 1024, height: 768, name: 'chrome'},
     });
     cy.eyesCheckWindow({
-      target: 'region',
-      selector: '.section',
       tag: 'Abra pendant',
     });
     cy.eyesClose();
