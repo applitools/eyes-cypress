e898759f2c266a51c5307d43b8f36d5e82e586db
Daniel Schwartz <danielsch24@gmail.com>
2019-07-21 16:42:44 +0300

    config file fix

---
+++ tests/fixtures/testApp/applitools.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apiKey: process.env.APPLITOOLS_API_KEY,
+  isDisabled: false,
+  serverUrl2: 'eyes.applitools.com',
+  concurrency: 10,
+  serverUrl2: 'https://eyesfabric3eyes.applitools.com',
+  // saveDebugData: false,
+  // showLogs: true,
+  // eyesTimeout: 1234,
+  // failCypressOnDiff: true,
+  // tapDirPath: './',
+  // browser: [{name: 'firefox'}],
+};
