59e5a48ef14679dbb617e898d1be1c8056191198
Daniel Schwartz <danielsch24@gmail.com>
2019-08-08 18:59:21 +0300

    updated vgc to better error handeling

--- tests/fixtures/testApp/cypress/integration-play/play.js
+++ tests/fixtures/testApp/cypress/integration-play/play.js
@@ -4,7 +4,9 @@
 
 describe('product-page-applitools', () => {
   it('Abra', () => {
-    cy.visit('https://www.google.com/', {failOnStatusCode: false});
+    cy.visit('https://www.asos.com/river-island/river-island-button-cowl-neck-jumper/prd/1775492', {
+      failOnStatusCode: false,
+    });
     cy.wait(1000);
     cy.eyesOpen({
       appName: 'product-page-applitools',
