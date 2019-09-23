cef3d506cc277499f040ae68d48a984c2f43cecd
Daniel Schwartz <danielsch24@gmail.com>
2019-09-23 11:25:34 +0300

    changes concurrency message again

--- src/plugin/concurrencyMsg.js
+++ src/plugin/concurrencyMsg.js
@@ -4,5 +4,5 @@ const MSG = `
 Important notice: Your Applitools visual tests are currently running with a concurrency value of 1.
 This means that the visual tests don't run in parallel, and therefore the execution might be slower.
 If your Applitools license supports a higher concurrency level, learn how to configure it here: https://www.npmjs.com/package/@applitools/eyes-cypress#concurrency.
-Need a higher concurrency in your account? Email us @ sdr@applitools.com with your required concurrency level`;
+Need a higher concurrency in your account? Email us @ sdr@applitools.com with your required concurrency level.`;
 module.exports = {concurrencyMsg: chalk.yellow(MSG), msgText: MSG};
