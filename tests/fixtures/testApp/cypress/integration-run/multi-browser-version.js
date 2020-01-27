e8d830c1b6a3e7712daef71815554ab60d8cbca3
Amit Zur <amit.zur@applitools.com>
2020-01-27 13:25:23 +0200

    update packages to get dom-snapshot fix

--- tests/fixtures/testApp/cypress/integration-run/multi-browser-version.js
+++ tests/fixtures/testApp/cypress/integration-run/multi-browser-version.js
@@ -1,24 +1,37 @@
 describe('eyes-cypress', () => {
 
-  // This also tests the setting of `testName` inside `it`
+  /*
+  NOTE: this test intentionally embeds the browser version into the test name. This is because of the "ping-pong problem":
+  2 browser versions share the same baseline. If there are visual differences between them, then one of them will be unresolved, and after
+  accepting the change in the dashboard, the other will be unresolved.
+  By embeding the browser version we avoid this issue, because every version will have its own baseline. But there's a price - every time there's
+  a browser version shift, we get a new baseline. So the test may lack coverage.
+  Eyes server should implement logic to make the browser version number part of the baseline, but if a version is missing a baseline - find a
+  baseline from a previous version of the same browser. Then, if the user saves a new baseline, save it under the new version, so in subsequent
+  runs the baseline for the new version would be found.
+  */
 
-  it('multi browser version', () => {
-    cy.setCookie('auth', 'secret');
-    const url = `http://localhost:${Cypress.config('testPort')}/test.html`;
-    cy.visit(url);
-    cy.eyesOpen({
-      appName: 'some app',
-      browser: [
-        {width: 1024, height: 768, name: 'chrome-one-version-back'},
-        {width: 1024, height: 768, name: 'chrome-two-versions-back'},
-        {width: 1024, height: 768, name: 'firefox-one-version-back'},
-        {width: 1024, height: 768, name: 'firefox-two-versions-back'},
-        // {width: 1024, height: 768, name: 'safari-1'},
-        // {width: 1024, height: 768, name: 'safari-2'},
-      ]
-      // showLogs: true,
+  const browsers = [
+    {width: 1024, height: 768, name: 'chrome-one-version-back'},
+    {width: 1024, height: 768, name: 'chrome-two-versions-back'},
+    {width: 1024, height: 768, name: 'firefox-one-version-back'},
+    {width: 1024, height: 768, name: 'firefox-two-versions-back'},
+    {width: 1024, height: 768, name: 'safari-one-version-back'},
+    {width: 1024, height: 768, name: 'safari-two-versions-back'},
+  ];
+
+  browsers.forEach(browser => {
+    it(`multi browser version: ${browser}`, () => {
+      cy.setCookie('auth', 'secret');
+      const url = `http://localhost:${Cypress.config('testPort')}/test.html`;
+      cy.visit(url);
+      cy.eyesOpen({
+        appName: 'some app',
+        browser
+        // showLogs: true,
+      });
+      cy.eyesCheckWindow();
+      cy.eyesClose();
     });
-    cy.eyesCheckWindow('multi browser');
-    cy.eyesClose();
-  });
+  })
 });
